23ec7df5a13623a4e5acc911ba736686d911d7b5
Karen Sanchez <karensnchzc@gmail.com>
2012-06-13 23:43:00 -0600

    Working on Invoice

--- BizConnect.Data/BizConnect.edmx.Designer.cs
+++
@@ -1,4 +0,0 @@
-﻿// Default code generation is disabled for model 'C:\Users\ksanchez\Documents\Neumont\Quarter 7\PRO390\MobileEnabledMvcApp\BizConnect.Data\BizConnect.edmx.old'. 
-// To enable default code generation, change the value of the 'Code Generation Strategy' designer
-// property to an alternate value. This property is available in the Properties Window when the model is
-// open in the designer.
